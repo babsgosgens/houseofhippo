6ca19b925e1a74e65deb7c7401b3ea742e9afa44
Babs Gösgens <babsgosgens@gmail.com>
2022-06-14 12:57:37 +0200

    Increase hitbox

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -48,29 +48,6 @@
         }
     });
 
-$(".filter li").on('click', function(){
-  var filter = $(this).data('filter');
-  $(".slider").slick('slickUnfilter');
-
-  if(filter == 'red'){
-    $(".slider").slick('slickFilter','.red');
-  }
-  else if(filter == 'yellow'){
-    $(".slider").slick('slickFilter','.yellow');
-  }
-  else if(filter == 'blue'){
-    $(".slider").slick('slickFilter','.blue');
-  }
-  else if(filter == 'all'){
-
-    $(".slider").slick('slickUnfilter');
-  }
-
-})
-
-
-
-
 
     $('.lazy').Lazy();
 
